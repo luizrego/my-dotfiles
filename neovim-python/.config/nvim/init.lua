7b6939553b5d1a18a0fc77831e0975dc838f48cd
Luiz <20259512+luizrego@users.noreply.github.com>
2025-02-18 16:14:37 +0000

    Added Python LSP and clipboard feature

--- neovim-python/.config/nvim/init.lua
+++ neovim-python/.config/nvim/init.lua
@@ -1,2 +1,15 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.g.clipboard = {
+  name = "WslClipboard",
+  copy = {
+    ["+"] = "clip.exe",
+    ["*"] = "clip.exe",
+  },
+  paste = {
+    ["+"] = 'powershell.exe -Command [Console]::Out.Write($(Get-Clipboard -Raw).tostring():gsub("`r", ""))',
+    ["*"] = 'powershell.exe -Command [Console]::Out.Write($(Get-Clipboard -Raw).tostring():gsub("`r", ""))',
+  },
+  cache_enabled = 0,
+}
